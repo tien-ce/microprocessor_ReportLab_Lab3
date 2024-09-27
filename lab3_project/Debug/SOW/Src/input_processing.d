2e00031ace03925e79ab6175577c1593a031c24a
Tien CSE <tien.nguyen11102004@hcmut.edu.vn>
2024-09-27 15:52:16 +0700

    update proteus

--- lab3_project/Debug/SOW/Src/input_processing.d
+++ lab3_project/Debug/SOW/Src/input_processing.d
@@ -24,7 +24,10 @@ SOW/Src/input_processing.o: E:/SOW/Src/input_processing.c \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
- E:/SOW/Ins/input_reading.h
+ E:/SOW/Ins/input_reading.h E:/SOW/Ins/global.h \
+ E:/SOW/Ins/software_timer.h E:/SOW/Ins/automatic.h E:/SOW/Ins/manual.h \
+ E:/SOW/Ins/Traffic_light.h E:/SOW/Ins/button.h \
+ E:/SOW/Ins/input_processing.h
 
 E:/SOW/Ins/input_processing.h:
 
@@ -79,3 +82,17 @@ E:/SOW/Ins/input_processing.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
 
 E:/SOW/Ins/input_reading.h:
+
+E:/SOW/Ins/global.h:
+
+E:/SOW/Ins/software_timer.h:
+
+E:/SOW/Ins/automatic.h:
+
+E:/SOW/Ins/manual.h:
+
+E:/SOW/Ins/Traffic_light.h:
+
+E:/SOW/Ins/button.h:
+
+E:/SOW/Ins/input_processing.h:
